8b1de8f01a812244259e293e5474abed90088a5f
Mark Brian <markbkiunga2017@gmail.com>
2024-07-17 18:16:10 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.querySelector('h1');
+
+newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Mark is the champion';
